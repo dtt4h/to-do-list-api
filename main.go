5bff6b64a40d043f174329e10197b37e1b8a1caa
dmitry <dtt4h322@gmail.com>
2025-08-22 02:02:24 +0700

    docs: adding based GET & POST requests

--- main.go
+++ main.go
@@ -1,9 +1,51 @@
 package main
 
 import (
-	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
+type Task struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Status      string `json:"status"` // done/in_progress/pending
+}
+
+var tasks = []Task{
+	{ID: 1, Title: "Buy a new pc", Description: "earn money", Status: "pending"},
+}
+
+func getTasks(c *gin.Context) {
+	c.JSON(http.StatusOK, tasks)
+}
+
+func createTask(c *gin.Context) {
+	var newTask Task
+
+	if err := c.ShouldBindJSON(&newTask); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	newTask.ID = len(tasks) + 1
+
+	tasks = append(tasks, newTask)
+
+	c.JSON(http.StatusCreated, gin.H{
+		"task": newTask,
+	})
+}
+
 func main() {
-	fmt.Println("before i forget")
+	r := gin.Default()
+
+	r.GET("/tasks", getTasks)
+
+	r.POST("/tasks", createTask)
+
+	r.Run()
 }
